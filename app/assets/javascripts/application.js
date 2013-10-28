b2fb52cab281213abcf695e8852820d51e7a9049
Chippy <sdk58@case.edu>
2013-10-27 23:06:08 -0400

    Template JS and CSS files moved into Vendor Assests files. Header/Footer/any application calls within main HTML pages taken away.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,4 +3,13 @@
 //= require turbolinks
 //= require_tree .
 //= require twitter/bootstrap
-//= require
+
+// Vendor Files (Plain Template)
+//= require functions
+//= require functions.min
+//= require jflickrfeed
+//= require jquery.hotkeys.min
+//= require jquery.isotope.min
+//= require jquery.touchSwipe.min
+//= require jquery-1.10.2.min
+//= require less-1.3.0.min
