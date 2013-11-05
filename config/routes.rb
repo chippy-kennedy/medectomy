e639756f3c93f5c3af76c2ea9b3f839c0be56944
chippy-kennedy <sdk58@case.edu>
2013-11-04 23:52:29 -0500

    adding all files to prelaunch-deployment branch

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Medectomy::Application.routes.draw do
   post '/subscribe', to: 'prelaunch#subscribe'
   resources :subscribers
   get '/contact', to: 'prelaunch#contact'
+  get '/product', to: 'prelaunch#product'
+  get '/index', to: 'prelaunch#index'
    root to: 'prelaunch#index'
 end
