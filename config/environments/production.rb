d5ee5ea334ec63b8fb2c1ec96e4c9e8195894f6d
chippy-kennedy <sdk58@case.edu>
2013-11-07 01:47:10 -0500

    more production.rb changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,9 @@ Medectomy::Application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
     
+    # Don't fallback to assets pipeline if a    precompiled asset is missed
+config.assets.compile = true
+    
   
 
   # Eager load code on boot. This eager loads most of Rails and
@@ -21,11 +24,8 @@ Medectomy::Application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
-  #CSS Files not loading -- Chippy's attempt to fix that issue
-  config.serve_static_assets = true
-    
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  #config.serve_static_assets = false
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
