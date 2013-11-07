1f3f48f89713b8e68df91c205b4e9a7447c611fa
chippy-kennedy <sdk58@case.edu>
2013-11-07 12:45:12 -0500

    added specific precompile command in aapplication.rb file to pull the prelaunch CSS.SCSS file. This is a test run in an attempt to load all assets this way

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,9 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+#specify what assets need to be precompiled
+config.assets.precompile += ['assets/stylesheets/prelaunch.css.scss']
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
