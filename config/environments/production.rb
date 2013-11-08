f0882322597e92af3845a1d7195ae0d1e97371e4
Abdallah Soliman <ass51@case.edu>
2013-11-08 13:30:57 -0500

    fixed last change to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Medectomy::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-   config.assets.precompile += %w( prelaunch.css )
+   config.assets.precompile += ['prelaunch.css']
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
