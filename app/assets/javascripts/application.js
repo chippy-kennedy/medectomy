fa3bb65ab670056808a0d5f26dd146d3bde86526
Abdallah Soliman <ass51@case.edu>
2014-03-02 14:16:36 -0500

    integrating template

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,13 @@
 //= require jquery
 //= require jquery_ujs
+//= require jquery.ui.all
+
 //= require turbolinks
 //= require bootstrap 
 //= require_tree .
+
+// Vendor Assets
+//= require_tree ../../../vendor/assets/javascripts/.
+
+
 
