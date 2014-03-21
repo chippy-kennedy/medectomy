008fce86ccfb70b6cfdda09b7087ff3144efce91
chippy-kennedy <sdk58@case.edu>
2014-03-21 13:51:28 -0400

    moved setting changes from tab in profile to a devise page (linked to from an icon in the user profile). TODO: Need to add a top margin so devise messages appear below the header 2)replace settings tab in profile with something else

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,16 @@ class UsersController < ApplicationController
 	skip_authorization_check
 
 	def index
-    @enrollments = current_user.enrollments
+    	@enrollments = current_user.enrollments
 		render 'users/index'
 	end
+
+	
+
+	def edit
+	 	@user = User.find(params[:id])
+  	end
+
+
 
 end
