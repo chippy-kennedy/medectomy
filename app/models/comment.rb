6dd435150840dc1b1cabf6dfb29a3946739bd556
MJ Harkins <mph47@case.edu>
2014-03-30 20:04:39 -0400

    removed my stupid shit

--- app/models/comment.rb
+++
@@ -1,3 +0,0 @@
-class Comment < ActiveRecord::Base
-	has_ancestry
-end
--- db/migrate/201404300025013_add_ancestry_to_message.rb
+++
@@ -1,5 +0,0 @@
-class AddAncestryToMessage < ActiveRecord::Migration
-  def change
-    add_column :messages, :ancestry, :string
-  end
-end
