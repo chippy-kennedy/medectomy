f94c51087cac53ccde458795c504b5f5668272ec
chippy-kennedy <sdk58@case.edu>
2014-04-25 14:38:03 -0400

    fixed root to go to user profile and fixed right sidebar buttons accordingly, add class [year] to user profile, and started to work on social button integration

--- config/routes.rb
+++ config/routes.rb
@@ -7,20 +7,37 @@ Medectomy::Application.routes.draw do
   
   resources :enrollments
 
+  
+  #Change root to user/index if logged in
+  authenticated :user do
+    root to: "users#index", as: :authenticated_root
+  end
+
+  unauthenticated do
+    root to: "pages#home", as: :unauthenticated_root
+  end
 
   # route for user profile
   #TODO: Below not working during intial signin
   get 'profile', :to => 'users#index'
+  get 'home', :to => 'pages#home'
 
-  root to: 'pages#home'
+
+  #root :to => 'users#index'
+  root :to => 'pages#home'
+
 	
   # routes for navigation on front pages 
   get 'available_courses', :to => 'pages#available_courses'
+
+
   get 'contact', :to => 'pages#contact'
 
   #special route for cancelling account
   #get 'cancel_account', :to => 'devise/registrations#cancel'
 
+   
 
+  
 
 end
